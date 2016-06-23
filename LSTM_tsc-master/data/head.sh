807319c3f76332225ca3cacf764ccafac2cb8435
szukiyu <szukiyu@gmail.com>
2016-06-23 17:54:11 +0900

    160623_2

---
+++ LSTM_tsc-master/data/head.sh
@@ -0,0 +1,4 @@
+shuf TEST_batch_norm |head -4000 > TEST_batch_norm4000
+shuf TEST_batch_abn |head -4000 > TEST_batch_abn4000
+shuf TRAIN_batch_norm |head -1000 > TRAIN_batch_norm1000
+shuf TRAIN_batch_abn |head -1000 > TRAIN_batch_abn1000
